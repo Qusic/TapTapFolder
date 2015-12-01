f64a17c3b473f5bb76a6a2de81ea2a5cae420178
Qusic <hi@qusic.me>
2015-12-01 17:03:58 +0800

    fix preferences for iOS 6 and earlier

--- Tweak.m
+++ Tweak.m
@@ -1,6 +1,9 @@
 #import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
 #import <UIKit/UIKit.h>
 #import <CaptainHook.h>
+
+extern void _CFXPreferencesRegisterDefaultValues(CFDictionaryRef defaultValues);
 
 @interface _UITapticEngine : NSObject
 - (void)actuateFeedback:(NSInteger)count;
@@ -55,7 +58,6 @@ static NSString * const kKeepFolderPreviewKey = @"KeepFolderPreview";
 static NSString * const kUse3DTouchKey = @"Use3DTouch";
 static NSString * const kDoubleTapTimeoutKey = @"DoubleTapTimeout";
 
-static NSUserDefaults *preferences;
 static SBIconView *tappedIcon;
 static NSDate *lastTappedTime;
 static BOOL doubleTapRecognized;
@@ -63,6 +65,29 @@ static BOOL doubleTapRecognized;
 CHDeclareClass(SBIconController)
 CHDeclareClass(SBIconView)
 CHDeclareClass(SBIconGridImage)
+
+static void registerPreferenceDefaultValues(void) {
+    _CFXPreferencesRegisterDefaultValues((__bridge CFDictionaryRef)@{
+        kReversedBehaviorKey: @YES,
+        kKeepFolderPreviewKey: @YES,
+        kUse3DTouchKey: @YES,
+        kDoubleTapTimeoutKey: @0.2
+    });
+}
+
+static BOOL getPreferenceBoolValue(NSString *key) {
+    return CFPreferencesGetAppBooleanValue((__bridge CFStringRef)key, (__bridge CFStringRef)kIdentifier, NULL);
+}
+
+static float getPreferenceFloatValue(NSString *key) {
+    float result = 0;
+    CFPropertyListRef value = CFPreferencesCopyAppValue((__bridge CFStringRef)key, (__bridge CFStringRef)kIdentifier);
+    if (value && CFGetTypeID(value) == CFNumberGetTypeID() && CFNumberIsFloatType(value)) {
+        CFNumberGetValue(value, kCFNumberFloatType, &result);
+        CFRelease(value);
+    }
+    return result;
+}
 
 static CGRect iconFrameForGridIndex(NSUInteger index) {
     CGFloat iconSize = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ? 45 : 54;
@@ -79,7 +104,7 @@ static BOOL isFolderIconView(SBIconView *view) {
 }
 
 static BOOL is3DTouchEnabled(SBIconView *view) {
-    return [preferences boolForKey:kUse3DTouchKey] && [view respondsToSelector:@selector(traitCollection)] && [view.traitCollection respondsToSelector:@selector(forceTouchCapability)] && view.traitCollection.forceTouchCapability == UIForceTouchCapabilityAvailable;
+    return getPreferenceBoolValue(kUse3DTouchKey) && [view respondsToSelector:@selector(traitCollection)] && [view.traitCollection respondsToSelector:@selector(forceTouchCapability)] && view.traitCollection.forceTouchCapability == UIForceTouchCapabilityAvailable;
 }
 
 static void launchFirstApp(SBIconView *iconView);
@@ -94,7 +119,7 @@ CHOptimizedMethod(1, self, void, SBIconController, iconTapped, SBIconView *, ico
         } else {
             NSDate *nowTime = [NSDate date];
             if (iconView == tappedIcon) {
-                if ([nowTime timeIntervalSinceDate:lastTappedTime] < [preferences floatForKey:kDoubleTapTimeoutKey]) {
+                if ([nowTime timeIntervalSinceDate:lastTappedTime] < getPreferenceFloatValue(kDoubleTapTimeoutKey)) {
                     doubleTapRecognized = YES;
                     doubleTapAction(iconView);
                     return;
@@ -107,7 +132,7 @@ CHOptimizedMethod(1, self, void, SBIconController, iconTapped, SBIconView *, ico
             tappedIcon = iconView;
             lastTappedTime = nowTime;
             doubleTapRecognized = NO;
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)([preferences floatForKey:kDoubleTapTimeoutKey] * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(getPreferenceFloatValue(kDoubleTapTimeoutKey) * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void) {
                 if (!doubleTapRecognized && iconView == tappedIcon) {
                     singleTapAction(iconView);
                 }
@@ -151,7 +176,7 @@ static void openFolder(SBIconView *iconView) {
 }
 
 static void singleTapAction(SBIconView *iconView) {
-    if ([preferences boolForKey:kReversedBehaviorKey]) {
+    if (getPreferenceBoolValue(kReversedBehaviorKey)) {
         openFolder(iconView);
     } else {
         launchFirstApp(iconView);
@@ -159,7 +184,7 @@ static void singleTapAction(SBIconView *iconView) {
 }
 
 static void doubleTapAction(SBIconView *iconView) {
-    if ([preferences boolForKey:kReversedBehaviorKey]) {
+    if (getPreferenceBoolValue(kReversedBehaviorKey)) {
         launchFirstApp(iconView);
     } else {
         openFolder(iconView);
@@ -167,26 +192,20 @@ static void doubleTapAction(SBIconView *iconView) {
 }
 
 CHOptimizedClassMethod(2, self, CGRect, SBIconGridImage, rectAtIndex, NSUInteger, index, maxCount, NSUInteger, count) {
-    return [preferences boolForKey:kKeepFolderPreviewKey]
+    return getPreferenceBoolValue(kKeepFolderPreviewKey)
         ? CHSuper(2, SBIconGridImage, rectAtIndex, index, maxCount, count)
         : iconFrameForGridIndex(index);
 }
 
 CHOptimizedClassMethod(3, self, CGRect, SBIconGridImage, rectAtIndex, NSUInteger, index, forImage, id, image, maxCount, NSUInteger, count) {
-    return [preferences boolForKey:kKeepFolderPreviewKey]
+    return getPreferenceBoolValue(kKeepFolderPreviewKey)
         ? CHSuper(3, SBIconGridImage, rectAtIndex, index, forImage, image, maxCount, count)
         : iconFrameForGridIndex(index);
 }
 
 CHConstructor {
     @autoreleasepool {
-        preferences = [[NSUserDefaults alloc]initWithSuiteName:kIdentifier];
-        [preferences registerDefaults:@{
-            kReversedBehaviorKey: @YES,
-            kKeepFolderPreviewKey: @YES,
-            kUse3DTouchKey: @YES,
-            kDoubleTapTimeoutKey: @0.2
-        }];
+        registerPreferenceDefaultValues();
         CHLoadLateClass(SBIconController);
         CHLoadLateClass(SBIconView);
         CHLoadLateClass(SBIconGridImage);
